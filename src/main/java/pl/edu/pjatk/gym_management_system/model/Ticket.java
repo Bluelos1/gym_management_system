39f1ea54a5ef7bb53013fa9954947e832bc912fe
Bluelos1 <s25946@pjwstk.edu.pl>
2022-11-20 20:24:14 +0100

    Git

---
+++ src/main/java/pl/edu/pjatk/gym_management_system/model/Ticket.java
@@ -0,0 +1,54 @@
+package pl.edu.pjatk.gym_management_system.model;
+
+import com.sun.istack.NotNull;
+import pl.edu.pjatk.gym_management_system.model.enums.TicketCategory;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "Start_date")
+    private LocalDate startDate;
+    @Column(name="End_date")
+    private LocalDate endDate;
+    @Column(name="Ticket_category")
+    private TicketCategory ticketCategory;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public TicketCategory getTicketCategory() {
+        return ticketCategory;
+    }
+
+    public void setTicketCategory(TicketCategory ticketCategory) {
+        this.ticketCategory = ticketCategory;
+    }
+}
---
+++ src/main/java/pl/edu/pjatk/gym_management_system/model/enums/Gender.java
@@ -0,0 +1,6 @@
+package pl.edu.pjatk.gym_management_system.model.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
